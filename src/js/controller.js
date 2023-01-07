24de4a2048731d9c641ea81ee736a80855bb4c36
cEmanuel26 <capatina_emanuel@yahoo.ro>
2023-01-07 21:40:29 +0100

    Added welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -112,6 +112,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to Tarkov');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -120,5 +124,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
