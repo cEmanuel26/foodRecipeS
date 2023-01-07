5990ce4ce592c42739ec723f7c522659c8b44d97
cEmanuel26 <capatina_emanuel@yahoo.ro>
2023-01-07 21:31:32 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3eb565b0-ce7a-4d1c-a6d5-a6cc7116c98f';
+export const MODAL_CLOSE_SEC = 2.5;
